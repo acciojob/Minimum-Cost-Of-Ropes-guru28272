55d727808c57ee5081a6a49e58c6ec4485e4dc94
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-02-28 19:47:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+const formEle = document.querySelector(`#form`);
+const inputVal = document.querySelector(`input`);
+const result = document.querySelector(`#result`);
+
+formEle.addEventListener(`click`, function (e) {
+  e.preventDefault();
+  let arr = inputVal.value.split(`,`);
+  getSorted(arr);
+
+  let res = 0;
+  let cost = 0;
+  while (arr.length > 1) {
+    res = Number(arr[0]) + Number(arr[1]);
+
+    arr.splice(0, 2);
+    arr.push(res);
+
+    cost += res;
+
+    getSorted(arr);
+  }
+  result.innerText = cost;
+});
+
+function getSorted(arr) {
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+}
